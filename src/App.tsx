02a70df29a19da01b902ceb612fa6dd0b4c87be6
bndambuki123 <bndambuki123@gmail.com>
2025-05-17 16:46:30 +0200

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -10,28 +10,24 @@ import { PrivacyPolicy } from './pages/PrivacyPolicy';
 import { TermsOfService } from './pages/TermsOfService';
 import { Toaster } from 'react-hot-toast';
 
-// function App() {
-//   return (
-//     <Router>
-//       <Toaster position="top-right" />
-//       <Layout>
-//         <Routes>
-//           <Route path="/" element={<Home />} />
-//           <Route path="/demo" element={<Demo />} />
-//           <Route path="/pricing" element={<Pricing />} />
-//           <Route path="/signup" element={<Signup />} />
-//           <Route path="/faq" element={<Faq />} />
-//           <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-//           <Route path="/terms-of-service" element={<TermsOfService />} />
-//         </Routes>
-//       </Layout>
-//     </Router>
-//   );
-// }
-
-// src/App.tsx
 function App() {
-  return <h1>Hello from Sasabot Beta!</h1>;
+  return (
+    <Router>
+      <Toaster position="top-right" />
+      <Layout>
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/demo" element={<Demo />} />
+{/*           <Route path="/pricing" element={<Pricing />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/faq" element={<Faq />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms-of-service" element={<TermsOfService />} /> */}
+        </Routes>
+      </Layout>
+    </Router>
+  );
 }
+
 
 export default App;
