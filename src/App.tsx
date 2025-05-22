c811fbf3ca48a1fcc89568aa9b196356a059c9d6
bndambuki123 <bndambuki123@gmail.com>
2025-05-22 16:42:49 +0300

    Add authentication and dashboard functionality

--- src/App.tsx
+++ src/App.tsx
@@ -8,25 +8,47 @@ import { Signup } from './pages/Signup';
 import { Faq } from './pages/Faq';
 import { PrivacyPolicy } from './pages/PrivacyPolicy';
 import { TermsOfService } from './pages/TermsOfService';
+import { Login } from './pages/Login';
+import { Dashboard } from './pages/Dashboard';
+import { FlowBuilder } from './pages/FlowBuilder';
+import { AuthProvider } from './components/AuthProvider';
+import { ProtectedRoute } from './components/ProtectedRoute';
 import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <Router>
-      <Toaster position="top-right" />
-      <Layout>
+      <AuthProvider>
+        <Toaster position="top-right" />
         <Routes>
-          <Route path="/" element={<Home />} />
-          <Route path="/demo" element={<Demo />} />
-          <Route path="/pricing" element={<Pricing />} />
-          <Route path="/signup" element={<Signup />} />
-          <Route path="/faq" element={<Faq />} />
-          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-          <Route path="/terms-of-service" element={<TermsOfService />} />
+          {/* Public routes */}
+          <Route element={<Layout />}>
+            <Route path="/" element={<Home />} />
+            <Route path="/demo" element={<Demo />} />
+            <Route path="/pricing" element={<Pricing />} />
+            <Route path="/signup" element={<Signup />} />
+            <Route path="/faq" element={<Faq />} />
+            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+            <Route path="/terms-of-service" element={<TermsOfService />} />
+            <Route path="/login" element={<Login />} />
+          </Route>
+
+          {/* Protected routes */}
+          <Route path="/dashboard" element={
+            <ProtectedRoute>
+              <Dashboard />
+            </ProtectedRoute>
+          } />
+          <Route path="/flow-builder" element={
+            <ProtectedRoute>
+              <FlowBuilder />
+            </ProtectedRoute>
+          } />
+
           {/* Catch all route for 404s */}
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
-      </Layout>
+      </AuthProvider>
     </Router>
   );
 }
