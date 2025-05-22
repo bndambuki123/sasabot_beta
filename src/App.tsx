0712c1dc82e20fde70fc99f11e70aafbe7d8f91e
bndambuki123 <bndambuki123@gmail.com>
2025-05-22 16:40:45 +0300

    Update dependencies and start server

--- src/App.tsx
+++ src/App.tsx
@@ -8,47 +8,25 @@ import { Signup } from './pages/Signup';
 import { Faq } from './pages/Faq';
 import { PrivacyPolicy } from './pages/PrivacyPolicy';
 import { TermsOfService } from './pages/TermsOfService';
-import { Login } from './pages/Login';
-import { Dashboard } from './pages/Dashboard';
-import { FlowBuilder } from './pages/FlowBuilder';
-import { AuthProvider } from './components/AuthProvider';
-import { ProtectedRoute } from './components/ProtectedRoute';
 import { Toaster } from 'react-hot-toast';
 
 function App() {
   return (
     <Router>
-      <AuthProvider>
-        <Toaster position="top-right" />
+      <Toaster position="top-right" />
+      <Layout>
         <Routes>
-          {/* Public routes */}
-          <Route element={<Layout />}>
-            <Route path="/" element={<Home />} />
-            <Route path="/demo" element={<Demo />} />
-            <Route path="/pricing" element={<Pricing />} />
-            <Route path="/signup" element={<Signup />} />
-            <Route path="/faq" element={<Faq />} />
-            <Route path="/privacy-policy" element={<PrivacyPolicy />} />
-            <Route path="/terms-of-service" element={<TermsOfService />} />
-            <Route path="/login" element={<Login />} />
-          </Route>
-
-          {/* Protected routes */}
-          <Route path="/dashboard" element={
-            <ProtectedRoute>
-              <Dashboard />
-            </ProtectedRoute>
-          } />
-          <Route path="/flow-builder" element={
-            <ProtectedRoute>
-              <FlowBuilder />
-            </ProtectedRoute>
-          } />
-
+          <Route path="/" element={<Home />} />
+          <Route path="/demo" element={<Demo />} />
+          <Route path="/pricing" element={<Pricing />} />
+          <Route path="/signup" element={<Signup />} />
+          <Route path="/faq" element={<Faq />} />
+          <Route path="/privacy-policy" element={<PrivacyPolicy />} />
+          <Route path="/terms-of-service" element={<TermsOfService />} />
           {/* Catch all route for 404s */}
           <Route path="*" element={<Navigate to="/" replace />} />
         </Routes>
-      </AuthProvider>
+      </Layout>
     </Router>
   );
 }
